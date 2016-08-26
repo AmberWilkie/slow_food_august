f0b58cfe7e5c5b07232a1008236d65b02765be2c
Amber <amber@amberwilkie.com>
2016-08-26 19:39:48 +0200

    Disallows display of invalid dishes

--- lib/models/menu.rb
+++ lib/models/menu.rb
@@ -13,6 +13,10 @@ class Menu
     # Need to make sure the Dish table is set up the same way I'm using the fake table in the spec file.
 
     @menu.each do |key, dish_hash|
+      if dish_hash[:name].nil?
+      elsif dish_hash[:price].nil?
+      elsif dish_hash[:category].nil?
+      else
         if dish_hash[:category] == "starter"
           @starters_array << dish_hash
         elsif dish_hash[:category] == "main"
@@ -20,6 +24,7 @@ class Menu
         elsif dish_hash[:category] == "dessert"
           @desserts_array << dish_hash
         end
+      end
     end
   end
 
--- spec/view_dishes_spec.rb
+++ spec/view_dishes_spec.rb
@@ -20,3 +20,22 @@ describe Menu do
 
 
 end
+
+describe 'Sad path for the Menu class' do
+    @fake_menu = {dish_one: {price: 80, category:"main"}, dish_two: {name: "Cinnamon Rolls", category:"dessert"}, dish_three: {name: "olives", price: 30}}
+    subject = Menu.new(@fake_menu)
+
+  it 'does not add dishes with empty name fields' do
+    expect(subject.mains_array).to be_empty
+  end
+
+  it 'does not add dishes with empty price fields' do
+    expect(subject.desserts_array).to be_empty
+  end
+
+  it 'does not add dishes with empty categories' do
+    expect(subject.starters_array).to be_empty
+  end
+
+
+end
