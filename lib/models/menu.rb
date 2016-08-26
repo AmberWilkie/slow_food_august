b2496cedd11610b6924a55fcfe82557cf43fc419
Amber <amber@amberwilkie.com>
2016-08-26 19:00:20 +0200

    Pull starter from dish list

--- lib/models/menu.rb
+++ lib/models/menu.rb
@@ -1,4 +1,23 @@
 class Menu
+  attr_accessor :starters_array
+
+  def initialize (menu)
+    @menu = menu
+    @starters_array = []
+    populate_menu_arrays
+  end
+
+  def populate_menu_arrays
+
+    @menu.each do |key, dish_hash|
+        if dish_hash[:category] == "starters"
+          @starters_array << dish_hash
+        end
+
+    end
+  end
+
+
 
 
 end
--- spec/view_dishes_spec.rb
+++ spec/view_dishes_spec.rb
@@ -1,12 +1,16 @@
-require './lib/menu'
+require './lib/models/menu'
 
-describe 'Menu' do
+describe Menu do
+  @fake_menu = {dish_one: {name: "sandwich", price: 80, category:"main"}, dish_two: {name: "Cinnamon Rolls", price: 90, category:"dessert"}, dish_three: {name: "olives", price: 30, category:"starters"}}
+  subject = described_class.new(@fake_menu)
+
   before do
-    # @menu = 
+
+
   end
 
-  it 'pulls a dish from the Dish table' do
-
+  it 'pulls a starter from the Dish table' do
+    expect(subject.starters_array[0][:name]).to eq "olives"
   end
 
 
