4b3bfb48b9f27e334f5d06bc4640f75c11a32a19
Amber <amber@amberwilkie.com>
2016-08-26 19:17:49 +0200

    Dessert shows up on menu page

--- lib/models/menu.rb
+++ lib/models/menu.rb
@@ -10,6 +10,7 @@ class Menu
   end
 
   def populate_menu_arrays
+    # Need to make sure the Dish table is set up the same way I'm using the fake table in the spec file.
 
     @menu.each do |key, dish_hash|
         if dish_hash[:category] == "starter"
@@ -19,12 +20,7 @@ class Menu
         elsif dish_hash[:category] == "dessert"
           @desserts_array << dish_hash
         end
-
-
     end
   end
-
-
-
 
 end
--- spec/view_dishes_spec.rb
+++ spec/view_dishes_spec.rb
@@ -1,13 +1,9 @@
 require './lib/models/menu'
 
 describe Menu do
+
   @fake_menu = {dish_one: {name: "sandwich", price: 80, category:"main"}, dish_two: {name: "Cinnamon Rolls", price: 90, category:"dessert"}, dish_three: {name: "olives", price: 30, category:"starter"}}
   subject = described_class.new(@fake_menu)
-
-  before do
-
-
-  end
 
   it 'pulls a starter from the Dish table' do
     expect(subject.starters_array[0][:name]).to eq "olives"
