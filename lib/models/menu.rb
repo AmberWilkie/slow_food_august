852de5b5dddafc60d3f82132db92cd7d22d8cfdc
Amber <amber@amberwilkie.com>
2016-08-26 19:03:53 +0200

    Pull main from dish array

--- lib/models/menu.rb
+++ lib/models/menu.rb
@@ -1,18 +1,22 @@
 class Menu
-  attr_accessor :starters_array
+  attr_accessor :starters_array, :mains_array
 
   def initialize (menu)
     @menu = menu
     @starters_array = []
+    @mains_array = []
     populate_menu_arrays
   end
 
   def populate_menu_arrays
 
     @menu.each do |key, dish_hash|
-        if dish_hash[:category] == "starters"
+        if dish_hash[:category] == "starter"
           @starters_array << dish_hash
+        elsif dish_hash[:category] == "main"
+          @mains_array << dish_hash
         end
+
 
     end
   end
--- spec/view_dishes_spec.rb
+++ spec/view_dishes_spec.rb
@@ -1,7 +1,7 @@
 require './lib/models/menu'
 
 describe Menu do
-  @fake_menu = {dish_one: {name: "sandwich", price: 80, category:"main"}, dish_two: {name: "Cinnamon Rolls", price: 90, category:"dessert"}, dish_three: {name: "olives", price: 30, category:"starters"}}
+  @fake_menu = {dish_one: {name: "sandwich", price: 80, category:"main"}, dish_two: {name: "Cinnamon Rolls", price: 90, category:"dessert"}, dish_three: {name: "olives", price: 30, category:"starter"}}
   subject = described_class.new(@fake_menu)
 
   before do
@@ -12,6 +12,15 @@ describe Menu do
   it 'pulls a starter from the Dish table' do
     expect(subject.starters_array[0][:name]).to eq "olives"
   end
+
+  it 'pulls a main from the Dish table' do
+    expect(subject.mains_array[0][:name]).to eq "sandwich"
+
+  end
+
+  # it 'pulls a dessert from the Dish table' do
+  #   expect(subject.starters_array[0][:name]).to eq "olives"
+  # end
 
 
 end
